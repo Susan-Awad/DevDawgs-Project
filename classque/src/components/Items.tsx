5903808bb1a9cdff1513a41f822f5fc79a61c79f
Susan.Awad <sta67748@uga.edu>
2025-04-13 20:10:59 -0400

    added title for UI

--- classque/src/components/Items.tsx
+++ classque/src/components/Items.tsx
@@ -34,13 +34,22 @@ import {useState, useEffect} from 'react';
  
                 {schedules.length === 0 ? (
                     <p>No schedules created yet. Let's get to work!</p>
-                ) : (
+                ) : (<>
+                    <div className="flex items-center justify-between mb-4">
+                        <h1 className="text-lg font-semibold mt-2">Your Schedules</h1>
+                        <Link href={`/create-item`}
+                            className="bg-[#6A3636] text-white px-4 py-2 rounded hover:bg-[#5A3636] mt-4 inline-block">
+                                Create
+                        </Link>
+                    </div>
+                    <hr/><br></br>
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
                         {schedules.map((item, k) => (  
                         <Item item={item} key={k}  />
                     ))}
                         
                     </div>
+                    </>
                 )}
             
             <Link href={`/create-item`}
