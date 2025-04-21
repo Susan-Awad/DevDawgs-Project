5bebae33f3a6734409af6adbdffce431e54d73b7
Jai Patel <jaipatel@Mac-Book.local>
2025-04-21 00:13:21 -0400

    Add LoginPage styling and component

--- classque/src/app/login/page.tsx
+++ classque/src/app/login/page.tsx
@@ -2,6 +2,7 @@ import LoginForm from '../../components/LoginForm';
 
 import Link from 'next/link'
 
+// Renders the login page with a styled container and LoginForm component.
 const LoginPage = () => {
   return (
     <div className="bg-[#D0A691] flex flex-col justify-center items-center p-10">
