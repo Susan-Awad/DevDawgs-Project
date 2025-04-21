371c5bdc09c638284a223afd0daba0989a5525b9
Jai Patel <jaipatel@Mac-Book.local>
2025-04-21 00:17:25 -0400

    fixed route.ts

--- classque/src/app/api/signup/route.ts
+++ classque/src/app/api/signup/route.ts
@@ -1,22 +1,19 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import User from "../../../models/userSchema";
-
 import bcrypt from "bcryptjs";
 import connectMongoDB from "../../../../config/mongodb";
 
-
-export const POST = async (request) => {
-  const {username, email, password} = await request.json();
-
-  console.log(username, email, password);
+export const POST = async (request: NextRequest) => {
+  const { username, email, password } = await request.json();
 
   await connectMongoDB();
   const hashedPassword = await bcrypt.hash(password, 5);
   const newUser = {
     username,
     password: hashedPassword,
-    email
-  }
+    email,
+  };
+
   try {
     await User.create(newUser);
   } catch (e: any) {
@@ -28,5 +25,4 @@ export const POST = async (request) => {
   return new NextResponse("User has been created", {
     status: 201,
   });
-
- }
+};
